83aec778694277d4fcb00b04a509eabb3ce27356
Khaldoun Kalash <kalach.9911@gmail.com>
2022-09-14 18:09:26 +0200

    projekt init

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UiModule } from "./ui/ui.moudle";
+
+@NgModule({
+  imports: [BrowserModule, AppRoutingModule, UiModule],
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
